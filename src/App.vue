3fd4f79e16120404d8e31c7fdf4b9d4f76f87477
iwasakitakehiro <iwasakitakehiro2@gmail.com>
2022-12-07 16:48:48 +0900

    created plain input form

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import DynamicForm from "./components/DynamicForm.vue";
+import { ref } from "vue";
+
+const formSchema = ref([
+  {
+    label: "お名前",
+    name: "name",
+    as: "input",
+  },
+  {
+    label: "ふりがな",
+    name: "ruby",
+    as: "input",
+  },
+  {
+    label: "メールアドレス",
+    name: "email",
+    as: "input",
+  },
+  {
+    label: "確認用メールアドレス",
+    name: "email",
+    as: "input",
+  },
+  {
+    label: "電話番号",
+    name: "tel",
+    as: "input",
+  },
+  {
+    label: "郵便番号",
+    name: "post-code",
+    as: "input",
+  },
+  {
+    label: "ご住所",
+    name: "address",
+    as: "input",
+  },
+  {
+    label: "お問い合わせ内容",
+    name: "note",
+    as: "textarea",
+  },
+]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <DynamicForm :schema="formSchema" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/DynamicForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { ref } from "vue";
+
+const props = defineProps({
+  schema: {
+    type: Object,
+    required: true,
+  },
+});
+const schema = ref(props.schema);
+</script>
+<template>
+  <Form>
+    <dl class="contact-table">
+      <dl v-for="field in schema" :key="field.name">
+        <dt :for="field.name">{{ field.label }}</dt>
+        <dd><Field :as="field.as" :id="field.name" :name="field.name" /></dd>
+      </dl>
+    </dl>
+    <button>Submit</button>
+  </Form>
+</template>
